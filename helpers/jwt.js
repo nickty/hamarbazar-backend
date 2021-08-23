fe2d7e39b61837a1bb5ee78b93cb5390542a0ffe
nickty <nickty.86@gmail.com>
2021-08-23 20:00:28 +0600

    users was missing in the register link

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -15,7 +15,7 @@ function authJwt() {
       { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
       { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
       '/api/v1/users/login',
-      '/api/v1/register',
+      '/api/v1/users/register',
     ],
   });
 }
